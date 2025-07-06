9d62c71c63dc7706b74c3f7a4b10fbcb1eae753d
tomo-taka108 <ikenocity@gmail.com>
2025-07-06 09:06:21 +0900

    初回コミット：READMEを追加＆Spring BootでFirst.Gitプロジェクト（hello worldを表示するWEBアプリ）を作成

---
+++ src/test/java/_practice/First/Git/ApplicationTests.java
@@ -0,0 +1,13 @@
+package _practice.First.Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
